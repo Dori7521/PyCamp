3e69aada87465492b54bc88c3b7e882739871774
Dori7521 <lelitodorota@gmail.com>
2022-04-08 15:30:04 +0200

    adding pylin and docstring

--- M0/lotek.py
+++ M0/lotek.py
@@ -1,22 +1,42 @@
-import random
+"""Lotto game
 
-my_nums = {1,40,7,30,21,19}
-cost = 3
+Returns:
+    int: cost and number of repetitions in the game
+"""
+
+from random import sample
+
+MY_NUMS = {1,40,7,30,21,19}
+COST = 3
 
 def random_generator():
-    return set(random.sample(range(1,50),k=6))
+    """Generates 6 random numbers to set
 
-def counter(my_nums,win_nums):
+    Returns:
+        set: six random numbers in range(1,50)
+    """
+    return set(sample(range(1,50),k=6))
+
+def counter(my_nums,rand_nums):
+    """Countes the number of repetitions
+
+    Args:
+        my_nums (set): my defined numbers
+        random_nums (function): does random_generator() function
+
+    Returns:
+        int: numbers of repetitions
+    """
+    win_nums = rand_nums()
     how_many=1
     while my_nums!=win_nums:
-        win_nums=random_generator()
+        win_nums=rand_nums()
         how_many+=1
-  
+
     return how_many
 
 if __name__=="__main__":
-    machine_nums = random_generator()
-    how_many = counter(my_nums,machine_nums)
-    print(f'You won after {how_many} attempts.')
-    total_cost=how_many*cost
-    print(f'You will pay {total_cost} PLN in total.')
+    HOW_MANY = counter(MY_NUMS,random_generator)
+    print(f'You won after {HOW_MANY} attempts.')
+    TOTAL_COST=HOW_MANY*COST
+    print(f'You will pay {TOTAL_COST} PLN in total.')
